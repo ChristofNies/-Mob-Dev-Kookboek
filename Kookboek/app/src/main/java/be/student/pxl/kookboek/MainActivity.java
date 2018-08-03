31a8a05fa75ab58a6601b7937414923c0d890b3b
ChristofNies <christof.nies@gmail.com>
2018-08-03 15:32:54 +0200

    home view created

--- Kookboek/app/src/main/java/be/student/pxl/kookboek/MainActivity.java
+++ Kookboek/app/src/main/java/be/student/pxl/kookboek/MainActivity.java
@@ -1,13 +1,21 @@
 package be.student.pxl.kookboek;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        toolbar.setTitle("Mijn Kookboek");
+        toolbar.setTitleTextColor(Color.WHITE);
+        setSupportActionBar(toolbar);
     }
 }
