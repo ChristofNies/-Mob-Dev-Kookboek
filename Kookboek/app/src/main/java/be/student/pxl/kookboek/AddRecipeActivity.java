6e83e62cb68b13cc46765ae61baf3e55e28789dd
ChristofNies <christof.nies@gmail.com>
2018-08-04 21:17:33 +0200

    add recipe view created

--- Kookboek/app/src/main/java/be/student/pxl/kookboek/AddRecipeActivity.java
+++ Kookboek/app/src/main/java/be/student/pxl/kookboek/AddRecipeActivity.java
@@ -5,14 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class AddRecipeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recipe);
         Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        toolbar.setTitle("Mijn Kookboek");
+        toolbar.setTitle("Recept toevoegen");
         toolbar.setTitleTextColor(Color.WHITE);
         setSupportActionBar(toolbar);
     }
